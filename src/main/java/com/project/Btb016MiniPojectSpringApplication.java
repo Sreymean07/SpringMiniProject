0f593adcf44a16bce3950da53bb2d7fcc3b7be25
Sreymean <thoeurn.sreymean07@gmail.com>
2017-06-21 10:42:52 -0700

    My First commit

---
+++ src/main/java/com/project/Btb016MiniPojectSpringApplication.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btb016MiniPojectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Btb016MiniPojectSpringApplication.class, args);
+	}
+}
